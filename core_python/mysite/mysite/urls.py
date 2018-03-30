268b69c1cb923e7b8e3182292111f4c20bd8a628
luyangong <luyangong@qq.com>
2018-03-30 17:19:51 +0800

    use general view

--- core_python/mysite/mysite/urls.py
+++ core_python/mysite/mysite/urls.py
@@ -15,8 +15,9 @@ Including another URLconf
 """
 from django.contrib import admin
 from django.urls import include, path
-
+from django.views.generic.base import RedirectView
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('blog/', include('blog.urls')), 
+    path('',RedirectView.as_view(url='/blog/'), name='go-to-blog'),
 ]
